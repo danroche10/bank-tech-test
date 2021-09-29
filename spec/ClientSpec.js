2e1609963f11bfb8fd4fd08c4ea9aa43e16168a4
danroche10 <danielroche10@gmail.com>
2021-09-29 09:07:17 +0100

    add more tests to clientSpec to make sure every line is being logged to console correctly

--- spec/ClientSpec.js
+++ spec/ClientSpec.js
@@ -2,13 +2,21 @@ describe('Client', function () {
   const Client = require('../src/Client');
   const AccountFactory = require('../src/factoryClasses/AccountFactory');
   const currentDate = new Date().toLocaleDateString();
+  const followingDay = new Date(new Date().getTime() + 86400000);
+  const twoDaysAfter = new Date(new Date().getTime() + 86400000 * 2);
   let client;
 
   beforeEach(function () {
     account = {
       addTransaction: () => {},
       accountStatement: () => {
-        return `date || credit || debit || balance\n${currentDate} || 2000.00 || || 2000.00\n${currentDate} || || 1000.00 || 1000.00`;
+        return [
+          `date || credit || debit || balance`,
+          `${currentDate} || 2000.00 || || 2000.00`,
+          `${currentDate} || || 1000.00 || 1000.00`,
+          `${followingDay} || || 1000.00 || 1000.00`,
+          `${twoDaysAfter} || 2000.00 || || 2000.00`,
+        ];
       },
     };
     spyOn(AccountFactory, 'createAccount').and.returnValue(account);
@@ -31,17 +39,51 @@ describe('Client', function () {
     });
   });
 
-  describe('prints client account statement in terminal', function () {
+  describe('console logs client account statement in terminal', function () {
     it('it calls accountStatement method from Account Class', function () {
       spyOn(account, 'accountStatement').and.returnValues(true);
       client.printStatement();
       expect(account.accountStatement).toHaveBeenCalled();
     });
 
-    it('console.logs stubbed method', function () {
+    it('console.logs account statement headers', function () {
       console.log = jasmine.createSpy('log');
       client.printStatement();
-      expect(console.log).toHaveBeenCalledWith(account.accountStatement());
+      expect(console.log).toHaveBeenCalledWith(
+        `date || credit || debit || balance`
+      );
+    });
+
+    it('console.logs first row of account statement', function () {
+      console.log = jasmine.createSpy('log');
+      client.printStatement();
+      expect(console.log).toHaveBeenCalledWith(
+        `${currentDate} || 2000.00 || || 2000.00`
+      );
+    });
+
+    it('console.logs second row of account statement', function () {
+      console.log = jasmine.createSpy('log');
+      client.printStatement();
+      expect(console.log).toHaveBeenCalledWith(
+        `${currentDate} || || 1000.00 || 1000.00`
+      );
+    });
+
+    it('console.logs third row of account statement', function () {
+      console.log = jasmine.createSpy('log');
+      client.printStatement();
+      expect(console.log).toHaveBeenCalledWith(
+        `${followingDay} || || 1000.00 || 1000.00`
+      );
+    });
+
+    it('console.logs fourth row of account statement', function () {
+      console.log = jasmine.createSpy('log');
+      client.printStatement();
+      expect(console.log).toHaveBeenCalledWith(
+        `${twoDaysAfter} || 2000.00 || || 2000.00`
+      );
     });
   });
 });
